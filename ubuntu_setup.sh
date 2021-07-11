fce9c083a39ffb8ba0a85d7246bc776179c37106
Greg Poisson <gregory.poisson2671@gmail.com>
2021-07-11 16:52:49 -0600

    fixed docker install

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,3 +1,7 @@
+release=$(lsb_release -cs)
+#if ($release == 'kali rolling')
+
+
 # Terminator Terminal
 sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update -y
@@ -19,9 +23,7 @@ sudo apt-get install -y \
     gnupg \
     lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu buster stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
