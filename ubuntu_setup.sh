cb7754677d4e1843cf5c2b6d03631ef34b19fa1e
Gregory Poisson <gregory.poisson2671@gmail.com>
2021-08-01 09:32:12 -0600

    update docker setup

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21,10 +21,10 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu buster stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce
 
 # Various tools
 sudo apt-get install -y cherrytree python3-pip git flameshot xclip mlocate
